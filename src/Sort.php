14549bf3fb9b9203563e37603394921998a9f9fe
LazyElePHPant <kevin@kevinpimentel.com>
2019-07-26 21:19:07 -0400

    Added selection sorting

--- src/Sort.php
+++ src/Sort.php
@@ -36,4 +36,33 @@ class Sort
 
 	return $numbers;
     }
+
+    public function selection(array $numbers) : array
+    {
+	for ($i = count($numbers) - 1; $i > 0; $i--) {
+	    $this->swap($numbers, $this->max($numbers, $i), $i);
+	}
+
+	return $numbers;
+    }
+
+    private function swap(array &$numbers, int $low, int $high)
+    {
+	$temp = $numbers[$low];
+	$numbers[$low] = $numbers[$high];
+	$numbers[$high] = $temp;
+    }
+
+    private function max(array $numbers, int $high)
+    {
+	$largest = 0;
+
+	for ($i = 1; $i <= $high; $i++) {
+	    if ($numbers[$largest] < $numbers[$i]) {
+		$largest = $i;
+	    }
+	}
+	
+	return $largest;
+    }
 }
--- tests/SortTest.php
+++ tests/SortTest.php
@@ -22,4 +22,13 @@ final class SortTest extends TestCase
 
 	$this->assertEquals([2,3,4,4,5,24,32,43,54,56,57,424,563,676,5677], $sort->insertion($list));
     }
+
+    public function test_that_a_list_can_be_sorted_using_selection_sort()
+    {	
+	$sort = new Sort();
+
+	$list = [676,2,56,24,5,3,4,563,54,57,5677,43,32,4,424];
+
+	$this->assertEquals([2,3,4,4,5,24,32,43,54,56,57,424,563,676,5677], $sort->selection($list));	
+    }
 }
